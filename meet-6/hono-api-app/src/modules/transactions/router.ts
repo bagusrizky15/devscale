2a4ba5da7a9bca28ba0539fc998e47b96b4c13b5
 Bagus Rizky 🦊 <mohamad_rizky@bca.co.id>
2026-02-18 15:49:05 +0700

    feat: add try catch

--- meet-6/hono-api-app/src/modules/transactions/router.ts
+++ meet-6/hono-api-app/src/modules/transactions/router.ts
@@ -5,98 +5,131 @@ import { prisma } from "../../utils/prisma.js";
 
 export const transactionsRouter = new Hono()
     .get("/", async (c) => {
-        const transactions = await prisma.transactions.findMany()
-
-        return c.json({
-            "message": "get all transactions",
-            "data": transactions
-        })
+        try {
+            const transactions = await prisma.transactions.findMany()
+            return c.json({
+                "message": "get all transactions",
+                "data": transactions
+            })
+        } catch (error) {
+            return c.json({
+                "message": "internal server error",
+                "error": error
+            }, 500)
+        }
     })
     .get("/:id", async (c) => {
+        try {
+            const transactionId = c.req.param("id")
+            const transaction = await prisma.transactions.findUnique({
+                where: {
+                    id: Number(transactionId)
+                }
+            })
 
-        const transactionId = c.req.param("id")
-        const transaction = await prisma.transactions.findUnique({
-            where: {
-                id: Number(transactionId)
+            if (!transaction) {
+                return c.json({
+                    "message": "transaction not found"
+                }, 404)
             }
-        })
 
-        if (!transaction) {
             return c.json({
-                "message": "transaction not found"
-            }, 404)
+                "message": "get transaction by id",
+                "data": transaction
+            })
+        } catch (error) {
+            return c.json({
+                "message": "internal server error",
+                "error": error
+            }, 500)
         }
-
-        return c.json({
-            "message": "get transaction by id",
-            "data": transaction
-        })
     })
     .post("/", zValidator("json", createTransactionSchema), async (c) => {
-        const body = c.req.valid("json")
-
-        const newTransaction = await prisma.transactions.create({
-            data: {
-                description: body.description,
-                price: body.price
-            }
-        })
-
-        return c.json({
-            "message": "success create new transaction",
-            "data": newTransaction
-        }, 201)
+        try {
+            const body = c.req.valid("json")
+            const newTransaction = await prisma.transactions.create({
+                data: {
+                    description: body.description,
+                    price: body.price
+                }
+            })
+            return c.json({
+                "message": "success create new transaction",
+                "data": newTransaction
+            }, 201)
+        } catch (error) {
+            return c.json({
+                "message": "internal server error",
+                "error": error
+            }, 500)
+        }
     })
     .patch("/:id", zValidator("json", createTransactionSchema.partial()), async (c) => {
-        const transactionId = c.req.param("id")
-        const body = c.req.valid("json")
+        try {
+            const transactionId = c.req.param("id")
+            const body = c.req.valid("json")
+            const extTransaction = await prisma.transactions.findUnique({
+                where: {
+                    id: Number(transactionId)
+                }
+            })
 
-        const extTransaction = await prisma.transactions.findUnique({
-            where: {
-                id: Number(transactionId)
+            if (!extTransaction) {
+                return c.json({
+                    "message": "transaction not found"
+                }, 404)
             }
-        })
 
-        if (!extTransaction) {
+            const updatedTransaction = await prisma.transactions.update({
+                where: {
+                    id: Number(transactionId)
+                },
+                data: {
+                    description: body.description,
+                    price: body.price
+                }
+            })
+
             return c.json({
-                "message": "transaction not found"
-            }, 404)
+                "message": "success update transaction",
+                "data": updatedTransaction
+            })
+        } catch (error) {
+            return c.json({
+                "message": "internal server error",
+                "error": error
+            }, 500)
         }
 
-        const updatedTransaction = await prisma.transactions.update({
-            where: {
-                id: Number(transactionId)
-            },
-            data: {
-                description: body.description,
-                price: body.price
-            }
-        })
-
-        return c.json({
-            "message": "success update transaction",
-            "data": updatedTransaction
-        })
     })
     .delete("/:id", async (c) => {
-        const transactionId = c.req.param("id")
+        try {
+            const transactionId = c.req.param("id")
+            const extTransaction = await prisma.transactions.findUnique({
+                where: {
+                    id: Number(transactionId)
+                }
+            })
 
-        const extTransaction = await prisma.transactions.findUnique({
-            where: {
-                id: Number(transactionId)
+            if (!extTransaction) {
+                return c.json({
+                    "message": "transaction not found"
+                }, 404)
             }
-        })
 
-        if (!extTransaction) {
+            await prisma.transactions.delete({
+                where: {
+                    id: Number(transactionId)
+                }
+            })
+
             return c.json({
-                "message": "transaction not found"
-            }, 404)
+                "message": "success delete transaction"
+            })
+        } catch (error) {
+            return c.json({
+                "message": "internal server error",
+                "error": error
+            }, 500)
         }
-
-        await prisma.transactions.delete({
-            where: {
-                id: Number(transactionId)
-            }
-        })
-        return c.json({ "message": "success delete transaction" })
     })
